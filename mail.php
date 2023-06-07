f5b58656bb6d486b56d31cce15d110132a681742
AndreyBelkov <89153934+AndreyBelkov@users.noreply.github.com>
2023-06-07 22:29:23 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -5,28 +5,34 @@ $mail = new PHPMailer;
 $mail->CharSet = 'utf-8';
 $mail->isHTML(true);
 
-$name = $_POST['form__name'];
-$nameChild = $_POST['form__name-child'];
-$childOld = $_POST['form__old-child'];
-$phone = $_POST['form__phone'];
-$email = $_POST['form__email'];
-//$description = $_POST['user_description'];
+
+if (isset($_POST['form__name'])) {$name = $_POST['form__name'];}
+if (isset($_POST['form__name-child'])) {$nameChild = $_POST['form__name-child'];}
+if (isset($_POST['form__old-child'])) {$childOld = $_POST['form__old-child'];}
+if (isset($_POST['form__phone'])) {$phone = $_POST['form__phone'];}
+if (isset($_POST['form__email'])) {$email = $_POST['form__email'];}
+if (isset($_POST['english-level'])) {$englishLevel = $_POST['english-level'];}
+if (isset($_POST['occupation'])) {$occupation = $_POST['occupation'];}
+if (isset($_POST['skill'])) {$skill = $_POST['skill'];}
+if (isset($_POST['business-shift'])) {$businessSmen = $_POST['business-shift'];}
+
+
 //business-intensive10@mail.ru   password:  uUsE1APd8op$
 
 
 $mail->isSMTP();                                    
 $mail->Host = 'smtp.mail.ru';  																							
 $mail->SMTPAuth = true;                               
-$mail->Username = 'business-intensive10@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'uUsE1APd8op$';
+$mail->Username = 'layoutdesigner@mail.ru'; 
+$mail->Password = 'pFgVu7nCPrLJyme0Xgyn';  
 $mail->SMTPSecure = 'ssl';                            
 $mail->Port = 465;
 
-$mail->setFrom('business-intensive10@mail.ru'); // от кого будет уходить письмо?
-$mail->addAddress('andrey_belkov@bk.ru');     // Кому будет уходить письмо 
+$mail->setFrom('layoutdesigner@mail.ru');
+$mail->addAddress('andrey_belkov@bk.ru');
 
-$mail->Subject = 'Заявка с сайта Развивающие каникулы 2023';                                
-$body = '<h1>Вам написали письмо!</h1>';
+$mail->Subject = 'Заявка с сайта Развивающие каникулы на бизнес интенсив';                                
+$body = '<h1>Запись ребенка на бизнес интенсив</h1>';
 if(trim(!empty($name))){
   $body.='<p><strong>Имя родителя:</strong> '.$name.'</p>';
 }
@@ -42,18 +48,20 @@ if(trim(!empty($phone))){
 if(trim(!empty($email))){
   $body.='<p><strong>Почта email:</strong> '.$email.'</p>';
 }
-/*if(trim(!empty($description))){
-  $body.='<p><strong>Дополнительные вопросы:</strong> '.$description.'</p>';
-}*/
+if(trim(!empty($businessSmen))){
+  $body.='<p><strong>Бизнес-смена:</strong> '.$businessSmen.'</p>';
+}
+if(trim(!empty($englishLevel))){
+  $body.='<p><strong>Уровень английского:</strong> '.$englishLevel.'</p>';
+}
+if(trim(!empty($occupation))){
+  $body.='<p><strong>Чем обычно занимается Ваш ребенок в свободное от школы время? Выбрано:</strong> '.$occupation.'</p>';
+}
+if(trim(!empty($skill))){
+  $body.='<p><strong>Какие навыки вы хотели бы развить в вашем ребенке больше всего? Выбрано:</strong> '.$skill.'</p>';
+}
 
-/*if(!empty($_FILES['user_file']['tmp_name'])){
-  $filePath = dirname(__FILE__) . '/' . $_FILES['user_file']['name'];
-  if(copy($_FILES['user_file']['tmp_name'], $filePath)){
-    $fileAttach = $filePath;
-    $body.='<p><strong>Фото в приложении</strong></p>';
-    $mail->addAttachment($fileAttach);
-  }
-}*/
+
 
 $mail->Body = $body;
 $mail->AltBody = '';
